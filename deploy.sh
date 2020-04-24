0619a5224cb3b6a4e715aaa7a46cc297b01ce683
hung <ndgnuh@protonmail.com>
2020-04-24 20:44:12 +0700

    some more custer f*ck, hope it works this time...

--- deploy.sh
+++ deploy.sh
@@ -11,8 +11,11 @@ echo "Fetching the sauce..."
 git clone --recursive https://github.com/ndgnuh/ndgnuh.github.io -b gh-pages blog
 cd blog
 
+echo "Fetching the website..."
+git clone https://github.com/ndgnuh/ndgnuh.github.io -b master public
+
 echo "Building the website with hugo-${HUGO_VERSION}..."
-hugo
+hugo --destination ../public
 
 if [ $? != 0 ]; then
     echo "Build failed"
@@ -24,13 +27,11 @@ touch ~/.git-credentials
 chmod 0600 ~/.git-credentials
 echo $GITHUB_AUTH_SECRET > ~/.git-credentials
 
+echo "Deploying to github..."
+cd public
 git config credential.helper store
 git config user.email "ndgnuh-blog-bot@users.noreply.github.com"
 git config user.name "ndgnuh-blog-bot"
-
-# push stuff on master
-echo "Deploying to github..."
-cd public/
 git add .
 git commit -m "Rebuild site at $(date "+%Y-%m-%d %H:%M")"
-git push --force origin HEAD:master
+git push origin HEAD:master
